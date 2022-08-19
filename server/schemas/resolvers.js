d6fb185c43e0911cf21f1914033405950ec368f5
bm <marcusb733@gmail.com>
2022-08-19 02:24:34 -0700

    Set Up Pages to user GQL Queries and Mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,10 +6,9 @@ const resolvers = {
     Query: {
         me: async (parent, args, context) => {
             if (context.user) {
-                const userData = await user
+                const userData = await User
                     .findOne({ _id: context.user._id })
                     .select("-__v -password")
-                    .populate("savedBooks");
 
                 return userData;
             }
